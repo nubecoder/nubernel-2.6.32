c68749dff683ef9a789d922bd36b2e03e445ae5e
Andi Kleen <andi@firstfloor.org>
2010-01-05 12:48:07 +0100

    driver-core: Add attribute argument to class_attribute show/store
    Passing the attribute to the low level IO functions allows all kinds
    of cleanups, by sharing low level IO code without requiring
    an own function for every piece of data.
    
    Also drivers can extend the attributes with own data fields
    and use that in the low level function.
    
    This makes the class attributes the same as sysdev_class attributes
    and plain attributes.
    
    This will allow further cleanups in drivers.
    
    Full tree sweep converting all users.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/net/bluetooth/sco.c
+++ Kernel/net/bluetooth/sco.c
@@ -971,7 +971,9 @@ drop:
 	return 0;
 }
 
-static ssize_t sco_sysfs_show(struct class *dev, char *buf)
+static ssize_t sco_sysfs_show(struct class *dev,
+				struct class_attribute *attr,
+				char *buf)
 {
 	struct sock *sk;
 	struct hlist_node *node;
