7e2b3ff0c0fc3058e8efaced3a5591eb4c68601e
Gustavo F. Padovan <padovan@profusion.mobi>
2010-05-01 16:15:35 -0300

    Bluetooth: Make hci_send_sco() void
    It also removes an unneeded check for the MTU. The check is done before
    on sco_send_frame()
    
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    Reviewed-by: João Paulo Rechi Vita <jprvita@profusion.mobi>
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/net/bluetooth/hci_core.c
+++ Kernel/net/bluetooth/hci_core.c
@@ -1279,17 +1279,12 @@ int hci_send_acl(struct hci_conn *conn, struct sk_buff *skb, __u16 flags)
 EXPORT_SYMBOL(hci_send_acl);
 
 /* Send SCO data */
-int hci_send_sco(struct hci_conn *conn, struct sk_buff *skb)
+void hci_send_sco(struct hci_conn *conn, struct sk_buff *skb)
 {
 	struct hci_dev *hdev = conn->hdev;
 	struct hci_sco_hdr hdr;
 
 	BT_DBG("%s len %d", hdev->name, skb->len);
-
-	if (skb->len > hdev->sco_mtu) {
-		kfree_skb(skb);
-		return -EINVAL;
-	}
 
 	hdr.handle = cpu_to_le16(conn->handle);
 	hdr.dlen   = skb->len;
@@ -1302,7 +1297,6 @@ int hci_send_sco(struct hci_conn *conn, struct sk_buff *skb)
 	bt_cb(skb)->pkt_type = HCI_SCODATA_PKT;
 	skb_queue_tail(&conn->data_q, skb);
 	hci_sched_tx(hdev);
-	return 0;
 }
 EXPORT_SYMBOL(hci_send_sco);
 
