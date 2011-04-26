0bbf309d6f4fc51bb068701799f9d274ab0002c2
nubecoder <nubecoder@gmail.com>
2011-04-26 02:31:53 -0400

    More script cleanup and changes.

--- wifi_kernel_flasher.sh
+++ wifi_kernel_flasher.sh
@@ -13,56 +13,72 @@
 #
 
 #define variables
-IP='192.168.1.187'
+IP="192.168.1.187"
 
 #define paths
-SRC=`pwd`/Kernel/arch/arm/boot/zImage
-DEST=/data/local/tmp/zImage
+ZIMAGE_SRC="$PWD/Kernel/arch/arm/boot/zImage"
+ZIMAGE_DEST="/data/local/tmp/zImage"
+
+BMLWRITE_SRC="$PWD/Kernel/update/bmlwrite"
+BMLWRITE_DEST="/data/local/tmp/bmlwrite"
+
+DST_PATH="/data/local/tmp/"
 
 #define cmds
-ADB_KILL='adb kill-server'
-ADB_CONNECT='adb connect'
-ADB_DISCONNECT='adb disconnect'
-ADB_SHELL='adb shell'
-ADB_PUSH='adb push'
-ADB_STATE='adb get-state'
+ADB_KILL="adb kill-server"
+ADB_CONNECT="adb connect"
+ADB_DISCONNECT="adb disconnect"
+ADB_SHELL="adb shell"
+ADB_PUSH="adb push"
+ADB_STATE="adb get-state"
 
 #error
-ERROR='no'
+ERROR="no"
 
 #redbend_ua cmd line usage
-REDBEND="redbend_ua restore $DEST /dev/block/bml7"
+REDBEND_CMD="redbend_ua restore $ZIMAGE_DEST /dev/block/bml7"
+BMLWRITE_CMD="bmlwrite $ZIMAGE_DEST /dev/block/bml7"
 
 #kill adb, start, and connect to wireless
-echo -e 'Killing adb server.'
+echo "Killing adb server."
 $ADB_KILL
-echo -e "Connect to $IP."
+echo "Connect to $IP."
 $ADB_CONNECT $IP
 
-# check for device (taken from the OneClickRoot)
+# check for device (taken from the OneClickRoot: http://forum.xda-developers.com/showthread.php?t=897612)
 CURSTATE=$($ADB_STATE | tr -d '\r\n[:blank:]')
-while [ "$CURSTATE" != device ]; do
+while [ "$CURSTATE" != device ];
+do
 	CURSTATE=$($ADB_STATE | tr -d '\r\n[:blank:]')
-	echo -e 'Phone is not connected.'
-	CURSTATE='device'
-	ERROR='yes'
+	echo "Phone is not connected."
+	CURSTATE="device"
+	ERROR="yes"
 done
 
-if [ "$ERROR" != yes ]; then
+if [ "$ERROR" != "yes" ];
+then
 	#remove previous kernel and push new one to phone
-	echo -e "Removing previous zImage from $DEST."
-	$ADB_SHELL 'rm' $DEST
-	echo -e 'Pushing file, this may take a minute.'
-	$ADB_PUSH $SRC $DEST
+	echo "Removing previous zImage from $ZIMAGE_DEST."
+	$ADB_SHELL "rm" $ZIMAGE_DEST
+	echo "Pushing zImage, this may take a minute."
+	$ADB_PUSH $ZIMAGE_SRC $ZIMAGE_DEST
+
+	#push bmlwrite to phone	and set permissions
+	echo "Pushing bmlwrite, this may take a minute."
+	$ADB_PUSH $BMLWRITE_SRC $BMLWRITE_DEST
+	echo "Setting permissions on bmlwrite (0755)."
+	$ADB_SHELL "chmod 0755" $BMLWRITE_DEST
 
 	#kick you to the adb shell
-	echo -e 'Kicking you to the adb shell.'
-	echo -e "usage: \n su\n $REDBEND"
+	echo "Kicking you to the adb shell."
+	echo "usage:\n su\n cd $DST_PATH\n $BMLWRITE_CMD"
+
+	#echo -e "usage:\n su\n $REDBEND_CMD"
 	$ADB_SHELL
 
 	#cleanup adb wireless by disconnecting
-	echo -e "Disconnect from $IP."
+	echo "Disconnect from $IP."
 	$ADB_DISCONNECT $IP
 else
-	echo -e "Please enable wireless adb and verify the IP matches: $IP."
+	echo "Please enable wireless adb and verify the IP matches: $IP."
 fi
