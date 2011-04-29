e3848e89595ffe1634f54212d89a16fd9b41a7d8
nubecoder <nubecoder@gmail.com>
2011-04-29 01:15:42 -0400

    Changes to branch4release.sh.

--- branch4release.sh
+++ branch4release.sh
@@ -11,8 +11,13 @@
 CURRENT_VERSION="nubernel-EC05_v0.0.0"
 VERSION_STRING="nubernel-EC05_v"
 
+# defaults
+RELEASE="n"
+FEATURE="n"
+
 # define vars
 NEW_VERSION=
+FEATURE_NAME=
 ERROR_MSG=
 
 # functions
@@ -20,15 +25,29 @@ SHOW_HELP()
 {
 	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
 	echo "Usage options for $0:"
-	echo "-v : Show verbose output while building zImage (kernel)."
-	echo "Example: -v 0.0.1."
+	echo "-f : Set the feature name and checkout new feature branch."
+	echo "     Example: -f overclock"
+	echo "-h : Print this help info."
+	echo "-v : Set the version and checkout new release branch."
+	echo "     Example: -v 0.0.1."
 	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
 	exit 1
 }
 SHOW_SETTINGS()
 {
 	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
-	echo "current version  == $VERSION_STRING"
+
+	if [ "$RELEASE" = "y" ]
+	then
+		echo "current version == $CURRENT_VERSION"
+		echo "new version     == ${VERSION_STRING}$NEW_VERSION"
+	fi
+
+	if [ "$FEATURE" = "y" ]
+	then
+		echo "feature name    == $FEATURE_NAME"
+	fi
+
 	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
 	echo "*"
 }
@@ -46,65 +65,90 @@ SHOW_ERROR()
 		echo "$ERROR_MSG"
 	fi
 }
-
-
-
-
-INCREMENT_VERSION()
+BRANCH_RELEASE()
 {
 	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
 	local T1=$(date +%s)
-	echo "Begin increment version..." && echo ""
-	#
-	#
-	#
-	local T2=$(date +%s)
-	echo "" && echo "Increment version took $(($T2 - $T1)) seconds."
-	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
-	echo "*"
-}
+	echo "Begin release branch..." && echo ""
 
-TEST()
-{
 	# checkout new release branch (always from dev)
 	git checkout -b release-v${NEW_VERSION} dev
 
 	# update files
 	PATTERN="$CURRENT_VERSION"
 	REPLACEMENT="${VERSION_STRING}$NEW_VERSION"
-	echo "$REPLACEMENT now in ncMultiBuild.sh"
 	sed -i "s/$PATTERN/$REPLACEMENT/g" ncMultiBuild.sh
-	echo "$REPLACEMENT now in build_kernel.sh"
-	sed -i "s/$PATTERN/$REPLACEMENT/g" build_kernel.sh
-	echo "$REPLACEMENT now in README"
+	sed -i "s/$PATTERN/$REPLACEMENT/g" README
+	sed -i "s/$PATTERN/$REPLACEMENT/g" $0
+	PATTERN="Changelog:"
+	REPLACEMENT="Changelog:\n\n"$(date +%d-%m-%Y)":\nCreated 'release-v${NEW_VERSION}' branch."
 	sed -i "s/$PATTERN/$REPLACEMENT/g" README
 
-	# update self
-	echo "$REPLACEMENT now in prepRelease.sh"
-	sed -i "s/$PATTERN/$REPLACEMENT/g" prepRelease.sh
-
-#	PATTERN="Changelog:"
-#	REPLACEMENT="Features:\n\nChangelog:"
-#	echo "Preforming action on README"
-#	sed -i "s/$PATTERN/$REPLACEMENT/g" README
-
-	#git add changes (but not README as that will need updating!)
+	#git add changes
 	git add ncMultiBuild.sh
-	git add build_kernel.sh
-	#git add README
-	#git add branch4release.sh
+	git add README
+	git add $0
 
-	# show git branches	
+	# show some info
+	git status -s
 	git branch
+
+	# git commit
+	git commit -m '"Created release-v'${NEW_VERSION}' branch."'
+
+	local T2=$(date +%s)
+	echo "" && echo "Release branch took $(($T2 - $T1)) seconds."
+	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
+	echo "*"
+}
+BRANCH_FEATURE()
+{
+	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
+	local T1=$(date +%s)
+	echo "Begin feature branch..." && echo ""
+
+	# checkout new feature branch (always from dev)
+	git checkout -b feature-${FEATURE_NAME} dev
+
+	# update files
+	PATTERN="Changelog:"
+	REPLACEMENT="Changelog:\n\n"$(date +%d-%m-%Y)":\nCreated 'feature-${FEATURE_NAME}' branch."
+	sed -i "s/$PATTERN/$REPLACEMENT/g" README
+
+	#git add changes
+	git add ncMultiBuild.sh
+	git add README
+	git add $0
+
+	# show some info
+	git status -s
+	git branch
+
+	# git commit
+	git commit -m '"Created feature-'${FEATURE_NAME}' branch."'
+
+	local T2=$(date +%s)
+	echo "" && echo "Feature branch took $(($T2 - $T1)) seconds."
+	echo "=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]=]"
+	echo "*"
 }
 
 
-
 # main
-while getopts  ":v:" flag
+while getopts  ":f:hv:" flag
 do
 	case "$flag" in
+	f)
+		FEATURE="y"
+		RELEASE="n"
+		FEATURE_NAME="$OPTARG"
+		;;
+	h)
+		SHOW_HELP
+		;;
 	v)
+		FEATURE="n"
+		RELEASE="y"
 		NEW_VERSION="$OPTARG"
 		;;
 	*)
@@ -115,8 +159,15 @@ do
 	esac
 done
 
-if [ -n "$NEW_VERSION" ] ; then
-	TEST
+SHOW_SETTINGS
+
+if [ "$RELEASE" = "y" ]
+then
+	BRANCH_RELEASE
+fi
+if [ "$FEATURE" = "y" ]
+then
+	BRANCH_FEATURE
 fi
 
 SHOW_COMPLETED
