a165b930d2c59f55d261925c03f2cab0cfdfdb7a
nubecoder <nubecoder@gmail.com>
2011-05-09 03:06:10 -0400

    Fixed up the wifiFlashHelper.sh and kernelFlash scripts a bit.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- scripts/wifiFlashHelper.sh
+++ scripts/wifiFlashHelper.sh
@@ -16,16 +16,20 @@
 IP="192.168.1.187"
 
 #define paths
+TMP_PATH="/data/local/tmp"
+
 ZIMAGE_SRC="$PWD/../Kernel/arch/arm/boot/zImage"
-ZIMAGE_DEST="/data/local/tmp/zImage"
+ZIMAGE_DEST="$TMP_PATH/zImage"
 
 REDBEND_SRC="$PWD/../initramfs/sbin/redbend_ua"
-REDBEND_DEST="/data/local/tmp/redbend_ua"
+REDBEND_DEST="$TMP_PATH/redbend_ua"
 
 BMLWRITE_SRC="$PWD/../initramfs/sbin/bmlwrite"
-BMLWRITE_DEST="/data/local/tmp/bmlwrite"
+BMLWRITE_DEST="$TMP_PATH/bmlwrite"
 
-DST_PATH="/data/local/tmp/"
+KERNELFLASH_SRC="$PWD/kernelFlash"
+KERNELFLASH_DEST="$TMP_PATH/kernelFlash"
+
 
 #define cmds
 ADB_KILL="adb kill-server"
@@ -35,18 +39,16 @@ ADB_SHELL="adb shell"
 ADB_PUSH="adb push"
 ADB_STATE="adb get-state"
 
+ADB_KERNEL_FLASH="su -c '/data/local/tmp/kernelFlash -k'"
+
 #error
 ERROR="no"
-
-#redbend_ua cmd line usage
-REDBEND_CMD="redbend_ua restore zImage /dev/block/bml7"
-BMLWRITE_CMD="bmlwrite zImage /dev/block/bml7"
 
 #kill adb, start, and connect to wireless
 echo "Killing adb server."
 $ADB_KILL
 echo "Connect to $IP."
-$ADB_CONNECT $IP > /dev/null
+$ADB_CONNECT $IP >/dev/null
 
 # check for device (taken from the OneClickRoot: http://forum.xda-developers.com/showthread.php?t=897612)
 CURSTATE=$($ADB_STATE | tr -d '\r\n[:blank:]')
@@ -65,27 +67,34 @@ then
 	$ADB_SHELL "rm" $ZIMAGE_DEST
 	$ADB_SHELL "rm" $REDBEND_DEST
 	$ADB_SHELL "rm" $BMLWRITE_DEST
+	$ADB_SHELL "rm" $KERNELFLASH_DEST
 
 	#push new kernel to phone
 	echo "Pushing zImage, this may take a minute."
-	$ADB_PUSH $ZIMAGE_SRC $ZIMAGE_DEST
-
-	#push redbend_ua to phone	and set permissions
+	$ADB_PUSH $ZIMAGE_SRC $ZIMAGE_DEST >/dev/null 2>&1
+	echo "*"
+	#push redbend_ua to phone and set permissions
 	echo "Pushing redbend_ua, this may take a minute."
-	$ADB_PUSH $REDBEND_SRC $REDBEND_DEST
+	$ADB_PUSH $REDBEND_SRC $REDBEND_DEST >/dev/null 2>&1
 	echo "Setting permissions on redbend_ua (0755)."
 	$ADB_SHELL "chmod 0755" $REDBEND_DEST
-
-	#push bmlwrite to phone	and set permissions
+	echo "*"
+	#push bmlwrite to phone and set permissions
 	echo "Pushing bmlwrite, this may take a minute."
-	$ADB_PUSH $BMLWRITE_SRC $BMLWRITE_DEST
+	$ADB_PUSH $BMLWRITE_SRC $BMLWRITE_DEST >/dev/null 2>&1
 	echo "Setting permissions on bmlwrite (0755)."
 	$ADB_SHELL "chmod 0755" $BMLWRITE_DEST
-
-	#kick you to the adb shell
-	echo "Kicking you to the adb shell."
-	echo -e "usage: \n cd $DST_PATH \n su \n $BMLWRITE_CMD \n or \n $REDBEND_CMD"
-	$ADB_SHELL
+	echo "*"
+	#push kernelFlash to phone and set permissions
+	echo "Pushing kernelFlash, this may take a minute."
+	$ADB_PUSH $KERNELFLASH_SRC $KERNELFLASH_DEST >/dev/null 2>&1
+	echo "Setting permissions on kernelFlash (0755)."
+	$ADB_SHELL "chmod 0755" $KERNELFLASH_DEST
+	echo "*"
+	#flash kernel with kernelFlash script
+	echo "Flashing kernel with kernelFlash -k."
+	echo "*"
+	$ADB_SHELL $ADB_KERNEL_FLASH
 
 	#cleanup adb wireless by disconnecting
 	echo "Disconnect from $IP."
