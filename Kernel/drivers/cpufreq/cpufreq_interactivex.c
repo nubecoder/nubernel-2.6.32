b953ef89b9b57ad0e3a6e65eb5be0d3f8667c75c
nubecoder <nubecoder@gmail.com>
2011-08-01 21:23:55 -0400

    Added some error checking to interactiveX governor.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/drivers/cpufreq/cpufreq_interactivex.c
+++ Kernel/drivers/cpufreq/cpufreq_interactivex.c
@@ -336,6 +336,7 @@ static int cpufreq_governor_interactivex(struct cpufreq_policy *new_policy,
 
 static int __init cpufreq_interactivex_init(void)
 {
+	int err;
 	unsigned int i;
 	struct timer_list *t;
 	min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
@@ -350,12 +351,25 @@ static int __init cpufreq_interactivex_init(void)
 
 	/* Scale up is high priority */
 	up_wq = create_rt_workqueue("kinteractivex_up");
+        if (!up_wq) {
+		printk(KERN_ERR "Creation of knteractivex_up failed\n");
+		return -EFAULT;
+	}
 	down_wq = create_workqueue("knteractivex_down");
+        if (!down_wq) {
+		printk(KERN_ERR "Creation of knteractivex_down failed\n");
+		return -EFAULT;
+	}
 
 	INIT_WORK(&freq_scale_work, cpufreq_interactivex_freq_change_time_work);
 
-        pr_info("[imoseyon] interactiveX enter\n");
-	return cpufreq_register_governor(&cpufreq_gov_interactivex);
+	pr_info("[imoseyon] interactiveX enter\n");
+	err = cpufreq_register_governor(&cpufreq_gov_interactivex);
+	if (err) {
+		destroy_workqueue(up_wq);
+		destroy_workqueue(down_wq);
+	}
+	return err;
 }
 
 #ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX
